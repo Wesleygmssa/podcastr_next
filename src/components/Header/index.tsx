7d9b4784bc81ca9baef65f095a4298c6163d29af
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-21 09:36:51 -0300

    Criando compnente header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
-import "./styles.module.scss";
-
+import styles from "./styles.module.scss";
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
 export function Header() {
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", { locale: ptBR });
   return (
-    <header className="headerContainer">
+    <header className={styles.headerContainer}>
       <img src="logo.svg" alt="podcast" />
       <p>O Melhor para você ouvir, sempre</p>
-      <span>Qui, 8 Abril</span>
+      <span>{currentDate}</span>
     </header>
   );
 }
