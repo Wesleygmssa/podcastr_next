f91ff41d3c671bf88ee8b62f3107a893e7bd9353
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-21 09:29:08 -0300

    Removendo estilos antigos

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import "./styles.module.scss";
+
 export function Header() {
   return (
-    <header>
+    <header className="headerContainer">
       <img src="logo.svg" alt="podcast" />
       <p>O Melhor para você ouvir, sempre</p>
       <span>Qui, 8 Abril</span>
