0ca1d4204f2bca737a6c2e66035594076155c9fa
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-20 21:41:32 -0300

    Criando Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <img src="logo.svg" alt="podcast" />
+      <p>O Melhor para você ouvir, sempre</p>
+      <span>Qui, 8 Abril</span>
+    </header>
+  );
+}
