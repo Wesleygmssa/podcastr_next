cf1b1757a85f56adbbacfd458dda5e7580c50d4a
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-21 11:53:50 -0300

    Criando chama static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
+//SPA
+//SSR
+//SSG => gera versão static
+
+import { useEffect } from "react";
+
+export default function Home(props) {
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/episodes")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+
   return <h1>Index</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
