7dceb07977c5ff8d315633af9acd27bd02d55525
Wesley Guerra <guerra_ssa@hotmail.com>
2021-04-21 15:37:26 -0300

    Api crianda com sucesso

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
-import { Header } from "../components/Header.tsx";
+//SPA
+//SSR
+//SSG
 
-export default function Home() {
-  return <div>Index</div>;
+import { useEffect } from "react";
+
+export default function Home(props) {
+  console.log(props);
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/episodes")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+  return <div>{JSON.stringify(props)}</div>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
